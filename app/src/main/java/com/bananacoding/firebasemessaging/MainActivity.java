447fd02dc29b520d236bdbfd23c15a49fc908fae
Teerapong Chantakard <azygous13@gmail.com>
2016-06-07 01:06:29 +0700

    Init project

---
+++ app/src/main/java/com/bananacoding/firebasemessaging/MainActivity.java
@@ -0,0 +1,27 @@
+package com.bananacoding.firebasemessaging;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseMessaging.getInstance().subscribeToTopic("news");
+
+        if (getIntent().getExtras() != null) {
+            for (String key : getIntent().getExtras().keySet()) {
+                String value = getIntent().getExtras().getString(key);
+                Log.d(TAG, "Key: " + key + " Value: " + value);
+            }
+        }
+    }
+}
